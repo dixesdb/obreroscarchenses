3e020c635c43139eb4f9b1d0d423e5ea574c0f9e
dixesdb <60619196+dixesdb@users.noreply.github.com>
2020-09-07 22:11:35 -0500

    Add files via upload

---
+++ java/js.js
@@ -0,0 +1,11 @@
+const cangre = document.querySelector('.cangre')
+const menu = document.querySelector('.menu-navegacion')
+
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', ()=>{
+   muni.classList.toggle("spread")
+})
